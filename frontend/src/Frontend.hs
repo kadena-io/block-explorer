3b1be509ec8bead7a48f50ee288d9b30e4c294cb
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-10-30 20:25:15 -0400

    add countdown

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -18,6 +18,7 @@ import           Control.Monad.Ref
 import           Data.Aeson
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import           Data.Fixed
 import           Data.Ord
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -206,15 +207,16 @@ blockTableWidget = do
     t <- liftIO getCurrentTime
     clockLossy 1 t
 
-  let elapsedTime gs ti = diffUTCTime (_tickInfo_lastUTC ti) (_gs_startTime gs)
+  let elapsedTime gs ti = max 0 $ diffUTCTime (_tickInfo_lastUTC ti) (_gs_startTime gs)
       elapsed = elapsedTime <$> stats <*> dti
       tps = calcTps <$> stats <*> elapsed
       hashrate = (\ti s -> calcNetworkHashrate (utcTimeToPOSIXSeconds $ _tickInfo_lastUTC ti) s) <$> dti <*> dbt
 
-  divClass "ui segment" $ divClass "ui three statistics" $ do
-    statistic "Est. Network Hash Rate" (dynText $ maybe "-" ((<>"/s") . diffStr) <$> hashrate)
-    statistic "Transactions Received" (dynText $ tshow . _gs_txCount <$> stats)
-    statistic "Current TPS" (dynText $ showTps <$> tps)
+  divClass "ui segment" $ do
+    divClass "ui small three statistics" $ do
+        statistic "Est. Network Hash Rate" (dynText $ maybe "-" ((<>"/s") . diffStr) <$> hashrate)
+        statistic "Transactions Received" (dynText $ tshow . _gs_txCount <$> stats)
+        statistic "Time until transactions are turned on" $ dynText (fmap f dti)
 
   divClass "block-table" $ do
     divClass "header-row" $ do
@@ -231,6 +233,15 @@ blockTableWidget = do
     return ()
   where
     dummy = TickInfo (UTCTime (ModifiedJulianDay 0) 0) 0 0
+    end = UTCTime (fromGregorian 2019 12 4) 0
+    f a = format $ convertToDHMS $ truncate $ diffUTCTime end (_tickInfo_lastUTC a)
+    format :: (Int, Int, Int, Int) -> Text
+    format (d, h, m, s) = T.pack $ printf "%d days %02d:%02d:%02d" d h m s
+    convertToDHMS t =
+      let (m', s) = divMod t 60
+          (h', m) = divMod m' 60
+          (d, h) = divMod h' 24
+      in (d, h, m , s)
 
 chainDifficulty :: ChainId -> BlockTable -> Text
 chainDifficulty cid bt =
