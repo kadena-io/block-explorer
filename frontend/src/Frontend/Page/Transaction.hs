de9388c8493b27b33597e58fec592eb94b1d0b09
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2023-03-28 14:01:14 -0400

    display tx info in a card

--- frontend/src/Frontend/Page/Transaction.hs
+++ frontend/src/Frontend/Page/Transaction.hs
@@ -13,6 +13,7 @@ module Frontend.Page.Transaction where
 
 ------------------------------------------------------------------------------
 import Control.Applicative ((<|>))
+import Control.Lens (iforM_)
 import Control.Monad
 import Data.Bool (bool)
 import Reflex.Dom.Core hiding (Value)
@@ -48,8 +49,10 @@ transactionPage netId _cid bp = do
     let isSuccess tout = case _toutResult tout of
           PactResult (Left _) -> False
           PactResult (Right _) -> True
-    let status tout = if isSuccess tout then elAttr "i" ("class" =: "green check icon" <> "title" =: "Succeeded") blank
-                else elAttr "i" ("class" =: "red close icon" <> "title" =: "Failed") blank
+    let status tout = do
+                elAttr "b" mempty (text "Status: ")
+                if isSuccess tout then elAttr "i" ("class" =: "green check icon" <> "title" =: "Succeeded") (text "Success")
+                    else elAttr "i" ("class" =: "red close icon" <> "title" =: "Failed") (text "Failure")
     let requestkey tx = hashB64U $ _transaction_hash tx
     let continuationCodeStep _tx = Nothing
     let continuationInitRequestKey tx = case _pactCommand_payload $ _transaction_cmd tx of
@@ -60,18 +63,25 @@ transactionPage netId _cid bp = do
           ContPayload _ -> Nothing
     let mkTxDetailRoute n reqKey = mkNetRoute n (NetRoute_TxDetail :/ reqKey)
     el "h2" $ text $ (tshow $ length txs) <> " Transactions"
-    elClass "table" "ui compact celled table" $ do
-       el "thead" $ el "tr" $ do
-        elAttr "th" ("style" =: "width:auto") $ text "Status"
-        elAttr "th" ("style" =: "width:auto") $ text "Request Key"
-        elAttr "th" ("style" =: "width:auto") $ text "Continuation"
-        elAttr "th" ("style" =: "width:auto") $ text "Code Preview"
-       el "tbody" $ do
-        forM_ txs $ \(t, tout) -> do
-          el "tr" $ do
-            elAttr "td" ("data-tooltip" =: bool "Failure" "Success" (isSuccess tout)) $ status tout
-            elAttr "td" ("class" =: "cut-text" <> "style" =: "max-width: 200px") $ routeLink (mkTxDetailRoute netId $ requestkey t) $ text $ requestkey t
-            elAttr "td" ("class" =: "cut-text" <> "style" =: "max-width: 200px") $ maybe (text "<No continuation>") text $ continuationInitRequestKey t
-            -- TODO: add continuation code preview
-            elAttr "td" ("class" =: "cut-text" <> "style" =: "max-width: 200px") $ maybe (text "no code to display") text $ (execCode t <|> continuationCodeStep t)
+    divClass "centered-container" $ iforM_ txs $ \i (t,tout) -> divClass "ui card" $ divClass "content" $ do
+            divClass "header" $ text $ "Transaction " <> tshow (succ i)
+            divClass "content" $ do
+              elAttr "h4" ("class" =: "ui sub header") $ text "Transaction Details"
+              divClass "ui small feed" $ do
+
+                divClass "summary" $ elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 200px") $ status tout
+                 
+                divClass "summary" $
+                  elAttr "span" ("data-tooltip" =: requestkey t) 
+                    $ elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 200px") $ do
+                        elAttr "b" mempty (text "Request Key: ")
+                        routeLink (mkTxDetailRoute netId $ requestkey t) $ text $ requestkey t
+
+                let contInfo = continuationInitRequestKey t
+                divClass "summary" $ elAttr "span" (foldMap ("data-tooltip" =:) contInfo) $
+                  elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 275px") $ do
+                    elAttr "b" mempty (text "Init Cont. Request Key: ")
+                    maybe (text "<No continuation>") text contInfo
+
+                divClass "summary" $ elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 300px") $ maybe (text "no code to display") text $ (execCode t <|> continuationCodeStep t)
 
