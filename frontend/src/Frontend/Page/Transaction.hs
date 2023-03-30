2f021f4d4ed2091c2547cf5ee8a3175b1503c843
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2023-03-30 11:21:48 -0400

    use compact celled table & not definition table

--- frontend/src/Frontend/Page/Transaction.hs
+++ frontend/src/Frontend/Page/Transaction.hs
@@ -59,40 +59,39 @@ transactionPage netId netConfig _cid bp = do
     let status tout = if isSuccess tout then elAttr "i" ("class" =: "green check icon" <> "title" =: "Succeeded") (text "Success")
                     else elAttr "i" ("class" =: "red close icon" <> "title" =: "Failed") (text "Failure")
     let requestkey tx = hashB64U $ _transaction_hash tx
-    let contExists tx = case _pactCommand_payload $ _transaction_cmd tx of
-          ExecPayload _ -> False
-          ContPayload _ -> True
-    let execCode tx = case _pactCommand_payload $ _transaction_cmd tx of
-          ExecPayload e -> Just $ _exec_code e
-          ContPayload _ -> Nothing
+    let onPayload f g tx = case _pactCommand_payload $ _transaction_cmd tx of
+          ExecPayload e -> f e
+          ContPayload c -> g c
+    let contExists = onPayload (const False) (const True)
+    let execCode = onPayload (Just . _exec_code) (const Nothing)
     let mkTxDetailRoute n reqKey = mkNetRoute n (NetRoute_TxDetail :/ reqKey)
     pb <- getPostBuild
 
     el "h2" $ text $ (tshow $ length txs) <> " Transactions"
-    elClass "table" "ui definition table" $ do
+    elClass "table" "ui compact celled table" $ do
       el "thead" $ el "tr" $ do
-        el "th" $ text "Transaction"
-        el "th" $ text "Status"
-        el "th" $ text "Code"
+        elAttr "th" ("style" =: "width: auto") $ text "Request Key"
+        elAttr "th" ("style" =: "width: auto") $ text "Status"
+        elAttr "th" ("style" =: "width: auto") $ text "Code"
       el "tbody" $ do
         forM_ txs $ \(t,tout) -> el "tr" $ do
           continuationCodeStep <- case contExists t of
             True -> do
               res <- getTxDetails netConfig (constDyn $ QParamSome $ RequestKey $ requestkey t) pb
-              let initialCode = (fmap (fmap (head . fmap _txDetail_initialCode)) res) -- there should only be one
+              let initialCode = fmap (fmap (head . fmap _txDetail_initialCode)) res -- there should only be one
               -- let previousSteps = (fmap (fmap (fmap _txDetail_previousSteps)) res)
               return initialCode
             False -> return never
-          el "td" $ do
-            elAttr "span" ("data-tooltip" =: requestkey t)
-              $ elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 200px") $
+          elAttr "td" (mempty) $ do
+            elAttr "span" ("data-tooltip" =: requestkey t) $
+              elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 200px;") $
                   routeLink (mkTxDetailRoute netId $ requestkey t) $ text $ requestkey t
           el "td" $ status tout
           let chainEmoji = "\x1F517"
               contToolTip = "This is a continuation of another transaction. The code is the initial code of the transaction that created this continuation."
           el "td" $ elAttr "span" (if contExists t then "data-tooltip" =: contToolTip else mempty) $
-            elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 200px")
-              $ (>>= dynText) $ holdDyn (fromMaybe "loading..." $ execCode t) $
+            elAttr "div" ("class" =: "cut-text" <> "style" =: "max-width: 200px;") $
+              (>>= dynText) $ holdDyn (fromMaybe "loading..." $ execCode t) $
                 continuationCodeStep <&> \case
                   Left err -> tshow $ "Error: " <> err
                   Right Nothing -> "No continuation"
